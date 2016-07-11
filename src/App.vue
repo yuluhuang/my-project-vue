e3d0dfdae637355e91d75a1e0ddc1fe8cad7a77e
yuluhuang <504367857@qq.com>
2016-07-11 21:58:25 +0800

    responsiveness

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,22 @@
 <template>
   <div id="app">
     <toolbar></toolbar>
-    <notes-list></notes-list>
-    <editor></editor>
+    <div class="">
+      <div class=" col-sm-6">
+        <div class="row">
+        <notes-list></notes-list>
+        </div>
+      </div>
+      <div class="col-sm-6">
+        <div class="row">
+        <editor></editor>
+          
+        </div>
+      </div>
+    </div>
+</div>
+
+    
   </div>
 </template>
 <style>
@@ -11,92 +25,22 @@
   #app {
     height: 100%;
   }
-  
-  body {
-    margin: 0;
-    padding: 0;
-    border: 0;
-    height: 100%;
-    max-height: 100%;
-    position: relative;
-  }
-  
-  #toolbar {
-    float: left;
-    width: 80px;
-    height: 100%;
-    background-color: #30414D;
-    color: #767676;
-    padding: 35px 25px 25px 25px;
-  }
-  
-  #notes-list {
-    float: left;
-    width: 300px;
-    height: 100%;
-    background-color: #F5F5F5;
-    font-family: 'Raleway', sans-serif;
-    font-weight: 400;
-  }
-  
-  #list-header {
-    padding: 5px 25px 25px 25px;
-  }
-  
-  #list-header h2 {
-    font-weight: 300;
-    text-transform: uppercase;
-    text-align: center;
-    font-size: 22px;
-    padding-bottom: 8px;
-  }
-  
-  #notes-list .container {
-    height: calc(100% - 137px);
-    max-height: calc(100% - 137px);
-    overflow: auto;
-    width: 100%;
-    padding: 0;
-  }
-  
-  #notes-list .container .list-group-item {
-    border: 0;
-    border-radius: 0;
-  }
-  
-  .list-group-item-heading {
-    font-weight: 300;
-    font-size: 15px;
-  }
-  
 
-  
-  #toolbar i {
-    font-size: 30px;
-    margin-bottom: 35px;
-    cursor: pointer;
-    opacity: 0.8;
-    transition: opacity 0.5s ease;
-  }
-  
-  #toolbar i:hover {
-    opacity: 1;
-  }
   
   .starred {
     color: #F7AE4F;
   }
 </style>
 <script>
-import Toolbar from './components/Toolbar.vue'
-import NotesList from './components/NotesList.vue'
-import Editor from './components/Editor.vue'
+  import Toolbar from './components/Toolbar.vue'
+  import NotesList from './components/NotesList.vue'
+  import Editor from './components/editor/Editor.vue'
 
-export default {
-  components: {
-    Toolbar,
-    NotesList,
-    Editor
+  export default {
+    components: {
+      Toolbar,
+      NotesList,
+      Editor
+    }
   }
-}
 </script>
